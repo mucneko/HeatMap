693e0cc35f812ddeaf5f8f26668c417a357eaaa0
neko <neko@greenie.muc.de>
2022-11-20 18:16:39 +0100

    geändert:       .gitignore nehme ich doch mit dazu
     	neue Datei:     conf/Config.conf
     	neue Datei:     conf/ORIG_Colors.map
    
    Hier Config-Dateien
    
     	geändert:       lib/Neko/HeatMap.pm
     	geändert:       t/HeatMap::getColorMap.t
    
    added setColorMap file-option
    
     	neue Datei:     tmp/getColorMap
     	neue Datei:     tmp/getColorMapFile
     	neue Datei:     tmp/getConfig
    zum manuellen aufrufen und Debugging.

--- lib/Neko/HeatMap.pm
+++ lib/Neko/HeatMap.pm
@@ -125,8 +125,415 @@ sub hasErrors {
     return $self->{'errormsg'};
 }
 
-sub createImage {}
-sub exportImage {}
+# HeatMap->createImage ( 'kuerzel' => 'MUC'|'GER'|'NRW', 
+#                        'infiles' => \@infiles,
+#                        'kw' => 1-53 #Kalenderwoche
+#                      );
+sub createImage {
+    my $self = shift;
+    my %args = @_;
+    my $kurz = $args{'kuerzel'};
+    my $kw = $args{'kw'};
+    my @infiles = @ { $args{'infiles'} };
+
+    my $Config = $self->getConfig();
+
+    # Config-Daten-Variablen
+
+    # Kuerzel - Langbezeichnung Zuordnung - wird im Bild oben verwendet
+    my $kurze = $Config->{'HeatMapOrte'};
+    my $ort = $kurze->{$kurz};
+
+
+    # Bildausgabe Breit/Hoehe
+    my $img_x = $Config->{'HeatMap'}{'img_x'} // 1200;
+    # my $img_y = 1050;
+    my $img_y = $Config->{'HeatMap'}{'img_y'} // 1150;
+
+    # Abstand von links und von oben zum Bildrand
+    my $offsetx = $Config->{'HeatMap'}{'offset_x'} // 50;
+    my $offsety = $Config->{'HeatMap'}{'offset_y'} // 40;
+
+    # Farbe vom Bildhintergrund (der ist sonst beim gif weiss und beim jpg schwarz)
+    # my $bgcolor = $Config->{'HeatMap'}{'bgcolor'};
+    my $bgcolor = Imager::Color->new( $Config->{'HeatMap'}{'bgcolor'} );
+
+    # einzelne Kasterl pro HeaatFarbe SizeX=Breite SizeY=Hoehe,
+    my $feldSizex = $Config->{'HeatMap'}{'feldSize_x'} // 20;
+    my $feldSizey = $Config->{'HeatMap'}{'feldSize_y'} // 40;
+    my $KW_fontSize = $feldSizex;
+
+    # ttf-Font von System verwenden 
+    my $font_file = $Config->{'HeatMap'}{'font_file'};
+    my $font = Imager::Font->new(file => $font_file ) or die;
+
+    # Fontgroesse fuer den Copyright-Text unten rechts
+    my $copyFont = $feldSizex;
+
+    # Farbschema
+    # $self->setColorMap( 'file' => $Config->{'HeatMapUser'}{'colorMap'} );
+    my $colors = $self->getColorMap();
+    
+    my @border_color = $Config->{'HeatMap'}{'border_color'};
+
+    my @rki = ();
+
+    # Datum und Output, Copyright (unten rechts im Bild)
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+    my $dat = sprintf("%04d-%02d-%02d", ($year+1900), ($mon +1), $mday );
+
+    my $copy = $dat. $Config->{'HeatMap'}{'copy'};
+    
+    print " internes... \n";
+    
+    # files der Reihe nach einlesen
+
+    foreach my $infile (@infiles){
+        open(IN ,"<:encoding(UTF-16)",$infile) || die "open File $infile - error $!\n";
+    
+        my $cnt = 0;
+        while(<IN>){
+            my $line = $_;
+            # $line =~ s/\"\"/"0,001"/g;
+            $line =~ s/\"\"/"-10"/g;
+            $line =~ s/\"//g;
+            my @arr = split /\s+/, $line;
+        
+    # Debugging
+    # print "rki:\n".Dumper (\@rki);
+    # print "arr:\n".Dumper (\@arr);
+    # print "cnt: $cnt \n";
+    
+            if ( $rki[$cnt] ) {
+                my @rki_arr = @{$rki[$cnt]};
+                my $c = 0;
+                # mit folgejahrdaten ueberschreiben bis zur angeg. kw oder Datenende
+                foreach my $f ( @arr ) {
+    
+    # print "cnt: $cnt - c: $c - kw: $kw\n";
+                    last if ($c > $kw);
+    
+    # print "ersetze $rki_arr[$c] mit $f\n";
+                    $rki_arr[$c] = $f;
+                    $c++;
+                }
+    
+                $rki[$cnt] = \@rki_arr;
+            }
+            else {
+                push @rki, \@arr;
+            }
+            $cnt++;
+    
+    # print "ende rki: \n".Dumper (\@rki);
+        }
+    
+        close IN;
+    }
+    
+    # print Dumper( \@rki );
+    
+    # die obersten beiden Zeilen sind nur die Wochen und Prosa - verwenden wir nicht
+    my $muell = shift @rki;
+    # print "entsorge shift ".Dumper($muell);
+    $muell = shift @rki;
+    # print "entsorge shift ".Dumper($muell);
+    
+    # Heatmap auf die hart Tour bauen
+    
+    
+    my $img = Imager->new(
+        xsize  => $img_x,        # Image width
+        ysize  => $img_y,        # Image height
+        channels => 4
+    );
+    $img->box(filled=>1, color=>$bgcolor );
+    
+    my @matrix = @rki;
+
+    my @point_datas = ();
+    
+    # print Dumper(\@point_datas);
+    
+    my $counter = 0;
+    # print Dumper(\@matrix);
+    
+    foreach my $p ( @matrix ) {
+    
+        $counter++;
+    
+    # print "p: ".Dumper($p) ."\n";
+    
+    
+        my $reihe = $counter;
+        my $y = $reihe * $feldSizey + $offsety;
+        my $y2 = $y + $feldSizey;
+        my $x2 = 0;
+
+        my $fc = 0; # Feldcounter
+
+        # Altersgruppe mal abfangen und aufheben
+        my $ag = shift @{$p};
+        $ag =~ s/^A//g;
+
+        # letzte Zeile hat das manchmal
+        next if ( uc($ag) eq 'UNBEKANNT' );
+
+        foreach my $t ( ($kw) .. ( scalar( @{$p} ) -1 ), 0 .. $kw-1 ) {
+            my @line = @{$p};
+    
+# if ( $t > 49 ){
+# print '@line: '."@line\n";
+# print 'scalar: ' .scalar( @line )."\n";
+# }
+
+            my $feld = $line[$t];
+
+# print '$feld: '.$feld."\n";
+# print '$t: '.$t."\n";
+
+        # Ausgabe auf der Shell letzte Spalte
+            if ( $t == $kw-1 ){ print "$ag: $feld\n"; }
+
+            $fc++;
+        # last if ( $fc > $kw );
+
+            my $weight = -10;
+        # die mit dem -1 nicht durchs berechnen schicken.
+        # if ( $feld ne '-1' ) {
+            # bei den Inzidenzen putzen, die kommen als 1.234,56
+            $feld =~ s/\.//g;
+            $feld =~ s/\,/\./g;
+
+# print "Farbe: $feld -> ";
+                $weight = nearest(10, $feld );
+                if ( $feld >= 100 ) {
+                    $weight = nearest(50, $feld );
+                }
+                if ( $feld >= 1000 ) {
+                    $weight = nearest(100, $feld );
+                }
+        # }
+# print "$weight\n";
+
+# print "Verwende Farbe $weight aus $feld\n";
+
+            my @color = @{$colors->{$weight}};
+
+            my $x = $fc * $feldSizex + $offsetx;
+            $x2 = $x + $feldSizex;
+
+
+    # print "Zeichne: $x, $y -> $x2, $y2 color: @color\n";
+       
+            my $fill = Imager::Fill->new(solid => \@color, combine => 'normal');
+            $img->box( xmin=> $x, ymin=> $y,
+                   xmax=> $x2, ymax=> $y2, 
+                   fill=> $fill
+                 );
+
+            # Kasterl drum - platt brutal gewachsen ohne Intelligenz
+            if ( $weight > 4200 ) {
+                my @mborder_color = $colors->{$weight -4200};
+
+                foreach my $i ( 9..10 ) {
+                    $img->box( color => @mborder_color, 
+                         xmin=> $x +$i, ymin=> $y +$i,
+                         xmax=>$x2 -$i, ymax=>$y2 -$i, 
+                         filled => 0,
+                         aa => 4);
+                }
+            }
+            elsif ( $weight > 2200 ) {
+                my @mborder_color = $colors->{$weight -2200};
+
+                foreach my $i ( 5..7 ) {
+                    $img->box( color => @mborder_color, 
+                         xmin=> $x +$i, ymin=> $y +$i,
+                         xmax=>$x2 -$i, ymax=>$y2 -$i, 
+                         filled => 0,
+                         aa => 4);
+                }
+            }
+            $img->box( color=> @border_color, xmin=> $x, ymin=> $y,
+                         xmax=>$x2, ymax=>$y2, filled => 0);
+        }
+
+        # Altersangabe hinten hin schreiben
+        $img->string(x => ($x2 + 4), y => $y2,
+                 font => $font,
+                 string =>  $ag,
+                 color => 'black',
+                 size => $KW_fontSize,
+                 aa => 1);
+
+    }
+
+#Labeln obere Reihe mit der KW-Angabe
+
+# KW
+$img->string( x => ($offsetx -( $feldSizex ) ), y => ( $offsety + ( $feldSizey -5 ) ),
+             font => $font,
+             string =>  'KW',
+             color => 'black',
+             size => $KW_fontSize,
+             aa => 1);
+
+# 1..53
+my $i = 0;
+foreach my $l ( $kw+1 .. ( scalar( @{$rki[3]} ) ), 1 .. $kw ){
+    $i++;
+    my $x = $i * $feldSizex + $offsetx;
+    $img->string(x => $x, y => ( $offsety + ( $feldSizey -5 ) ),
+             font => $font,
+             string =>  $l,
+             color => 'black',
+             size => $KW_fontSize,
+             aa => 1);
+}
+
+# Farb-Legende unten
+
+$i = '';
+my $c = 0;
+my $y = 20*$feldSizey + $offsety;
+# my $x = $feldSizex + $offsetx;
+my $x = $feldSizex;
+
+foreach my $l ( sort { $a <=> $b } ( keys %{$colors} ) ){
+
+    next if ( $l < 0 );
+
+    # $c++;
+
+    my $von = $l;
+       $von = $l - 5 if( $l>4);
+    my $bis = ($l*1 +4);
+    if ( $l >= 100 ) {
+            $von = $l - 25;
+            $bis = $l*1 +24;
+    }
+    if ( $l >= 1000 ) {
+            $von = $l - 50;
+            $bis = $l +49;
+    }
+
+    $i = "$von - $bis";
+    if ( $c +1 >= ( scalar ( keys %{$colors} ) -1 ) ) {   $i = " > $von"; }
+
+    $img->string(x => $x+$feldSizex+5 , y => $y -5,
+             font => $font,
+             string =>  $i,
+             color => 'black',
+             size => $feldSizex*0.75,
+             aa => 1);
+
+    my $fill = Imager::Fill->new(solid => $colors->{$l}, combine => 'normal');
+
+    $img->box( xmin=> $x, ymin=> $y -$feldSizey ,
+               xmax=> $x+$feldSizex, ymax=> $y ,
+               fill=> $fill 
+    );
+
+    # Kasterl drum - platt brutal gewachsen ohne Intelligenz
+    if ( $l > 4200 ) { 
+        my @mborder_color = $colors->{ ($l -4200) } ; 
+        
+        foreach my $i ( 9..10 ) {
+            $img->box( color=> @mborder_color,
+                  xmin=> $x +$i, ymin=> $y -$feldSizey +$i,
+                  xmax=> $x+$feldSizex -$i, ymax=> $y -$i,
+                  filled => 0 ,
+                  aa => 4);
+        }
+    }
+    if ( $l > 2200 ) { 
+        my @mborder_color = $colors->{ ($l -2200) } ; 
+        
+        foreach my $i ( 5..7 ) {
+            $img->box( color=> @mborder_color,
+                  xmin=> $x +$i, ymin=> $y -$feldSizey +$i,
+                  xmax=> $x+$feldSizex -$i, ymax=> $y -$i,
+                  filled => 0 ,
+                  aa => 4);
+        }
+    }
+
+    $img->box( color=> @border_color,
+                  xmin=> $x, ymin=> $y -$feldSizey,
+                  xmax=> $x+$feldSizex, ymax=> $y,
+                  filled => 0 ,
+                  aa => 4);
+
+    $c++;
+# print "c: $c\n";
+    # if ( ( $c % 5 ) == 0 ) {
+        # $x += nlowmult ( 1, ($c / 5) )  + ( $feldSizex*5 );
+        # $y = 19*$feldSizey + 2*$offsety;
+    # }
+    if ( ( $c % 8 ) == 0 ) {
+        # $x += nlowmult ( 1, ($c / 8) )  + ( $feldSizex*8 );
+        $x += nlowmult ( 1, ($c / 8) )  + ( $feldSizex*5 );
+        $y = 19*$feldSizey + 2*$offsety;
+    }
+
+    else {
+        $y = $y+$feldSizey;
+    }
+}
+
+
+#copyrighten
+# bottom right-hand corner of the image
+$img->align_string(x => $img->getwidth() - 1,
+                   y => $img->getheight() - 1,
+                   halign => 'right',
+                   valign => 'bottom',
+                   string => $copy,
+                     font => $font,
+                    color => 'black',
+                     size => $copyFont,
+                       aa => 1);
+ 
+# Noch die Ueberschrift drueber
+$img->string(x => ( ( $feldSizex*2)+$offsetx) , y => 40,
+             font => $font,
+             string => 'HeatMap Covid19, Inzidenzen, '.$ort.', nach Alter',
+             color => 'black',
+             size => $feldSizex*2,
+             aa => 3);
+ 
+}
+
+# $HeatMap->exportImage ( 'image' => Imager::new , 'filename' => $filename, 'format' => 'gif' )
+sub exportImage {
+    my $self = shift;
+    my %args = @_;
+    my $img = $args{'image'};
+    my $fn = $args{'filename'};
+    my $format = $args{'format'} // 'sgi';
+    my $file = 'platzhalter';
+    my $saved = 0;
+
+    # for my $f ( qw( gif jpeg ) ) {
+    # jpg is bigger than gif
+    for my $f ( qw( $format ) ) {
+
+        # Check if given format is supported
+        if ($Imager::formats{$f}) {
+            # my $fn = $file."_".   sprintf("%02d", $kw).".".$f;
+            $fn = $file.'.'.$f unless $fn;
+            print "Storing image as: $fn\n";
+ 
+            $img->write(file=>$fn) or
+# XXXXX hier Fehlerbehandlung statt die!
+                die $img->errstr;
+            $saved = 1;
+
+        }
+    }
+    
+}
 
 
 
@@ -587,15 +994,6 @@ foreach my $l ( sort { $a <=> $b } ( keys %{$colors} ) ){
 }
 
 
-# OutputFilename generieren
-if ( $use_this_filename ) {
-    $file = $use_this_filename;
-}
-else {
-    # Output Filename generieren 03:00 einfach hart anh‰ngen, das paﬂt meist schon, Sonderloecken via use_filename machen
-    $file = 'HeatMap_'.$kurz.'_'.(sprintf("%04d_%02d_%02d", ($year+1900), ($mon +1), $mday ));
-}
-
 #copyrighten
 # bottom right-hand corner of the image
 $img->align_string(x => $img->getwidth() - 1,
@@ -617,6 +1015,15 @@ $img->string(x => ( ( $feldSizex*2)+$offsetx) , y => 40,
              aa => 3);
  
 # speichern
+
+# OutputFilename generieren
+if ( $use_this_filename ) {
+    $file = $use_this_filename;
+}
+else {
+    # Output Filename generieren 03:00 einfach hart anh‰ngen, das paﬂt meist schon, Sonderloecken via use_filename machen
+    $file = 'HeatMap_'.$kurz.'_'.(sprintf("%04d_%02d_%02d", ($year+1900), ($mon +1), $mday ));
+}
 
 # sgi ging auf Mac von Haus aus, alles andere brauchte etwas Liebe (=libs)
 $format = 'sgi';
