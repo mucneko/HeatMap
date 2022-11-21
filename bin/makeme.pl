7c2b39569ce997e556d2c7893d75244d16a6b60f
neko <neko@greenie.muc.de>
2022-11-21 13:57:02 +0100

    geändert:       makeme.pl
    sollte jetzt tun und als muster herhalten können

--- bin/makeme.pl
+++ bin/makeme.pl
@@ -18,9 +18,11 @@ my $GER_2021 = 'survstat_GER_2021_2022_07_13';
 my $MUC = "survstat_MUC_$datum";
 my $GER = "survstat_GER_$datum";
 
+my $dataBase = '/Users/neko/Desktop/Covid/Inzidenzen';
+my $scriptBase = '/Users/neko/Desktop/Covid/Inzidenzen/bin/HeatMap/bin';
 my $configFile = '/Users/neko/Desktop/Covid/Inzidenzen/bin/HeatMap/conf/Config.conf';
 
-chdir '../'; 
+chdir $dataBase; 
 print `pwd`;
 
 foreach my $dest ( $MUC, $GER ){
@@ -30,16 +32,16 @@ foreach my $dest ( $MUC, $GER ){
         chdir $dest;
         print `$tar vxfz $dest.zip` ."\n";
         `rm $dest.zip`;
-        chdir '..';
+        chdir $scriptBase;
     }
 }
 
-chdir 'bin/';
+# chdir 'bin/';
 
 # print `pwd`;
 
-my $call1 = "./createHeatmap.pl -c $configFile -o MUC -w $kw -f HeatMap_MUC_$datum\.gif ../../../$MUC_2021/Data.csv ../../../$MUC/Data.csv" ;
-my $call2 = "./createHeatmap.pl -c $configFile -o GER -w $kw -f HeatMap_GER_$datum\.gif ../../../$GER_2021/Data.csv ../../../$GER/Data.csv" ;
+my $call1 = "./createHeatmap.pl -c $configFile -o MUC -w $kw -f HeatMap_MUC_$datum\.gif $dataBase/$MUC_2021/Data.csv $dataBase/$MUC/Data.csv" ;
+my $call2 = "./createHeatmap.pl -c $configFile -o GER -w $kw -f HeatMap_GER_$datum\.gif $dataBase/$GER_2021/Data.csv $dataBase/$GER/Data.csv" ;
 # my $call2 = "./createHeatmap.pl GER $kw HeatMap_GER_$datum ../$GER_2021/Data.csv ../$GER/Data.csv" ;
 print "call: ".$call1."\n";
 print `$call1`."\n";
@@ -47,8 +49,8 @@ print "call: ".$call2."\n";
 print `$call2`."\n";
 # print `./createHeatmap.pl GER $kw 'HeatMap_GER_$datum' ../$GER_2021/Data.csv ../$GER/Data.csv`."\n";
 
-# print `mv *MUC*.gif ../MUC_GIF/`."\n";
-# print `mv *GER*.gif ../GER_GIF/`."\n";
+print `mv *MUC*.gif $dataBase/MUC_GIF/`."\n";
+print `mv *GER*.gif $dataBase/GER_GIF/`."\n";
 # print `mv *MUC*.jpeg ../MUC_JPG/`."\n";
 # print `mv *GER*.jpeg ../GER_JPG/`."\n";
 
