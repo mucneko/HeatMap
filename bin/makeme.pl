35327b25a87e40dd8db62b4b932d24bd8d504100
neko <neko@greenie.muc.de>
2022-11-21 13:49:10 +0100

    Zum Commit vorgemerkte Änderungen:  a	geändert:       ../VERSION Prosa  	geändert:       makeme.pl zwischencommit

--- bin/makeme.pl
+++ bin/makeme.pl
@@ -1,33 +1,11 @@
 #!/usr/bin/perl
 
-package functions;
 use strict;
 use warnings;
 
-
-use strict;
-use warnings;
 
 use Data::Dumper;
 $Data::Dumper::Sortkeys = 1;   # Dumperdaten sortiert (sort) ausgeben
-
-use Encode 'encode';                    # UTF8<->8859-1 forcieren
-
-
-# Template Toolkit
-use Template;
-
-our (@ISA, @EXPORT);
-
-@ISA = qw(Exporter);
-
-@EXPORT = qw(
-              xxxx
-            );
-
-sub xxxx
-{
-}
 
 
 my $datum = shift @ARGV // die "kein Datum mitgegeben (z.B. 2022_04_03_0600)";
@@ -39,6 +17,8 @@ my $GER_2021 = 'survstat_GER_2021_2022_07_13';
 
 my $MUC = "survstat_MUC_$datum";
 my $GER = "survstat_GER_$datum";
+
+my $configFile = '/Users/neko/Desktop/Covid/Inzidenzen/bin/HeatMap/conf/Config.conf';
 
 chdir '../'; 
 print `pwd`;
@@ -58,16 +38,17 @@ chdir 'bin/';
 
 # print `pwd`;
 
-my $call1 = "./createHeatmap.pl MUC $kw HeatMap_MUC_$datum ../$MUC_2021/Data.csv ../$MUC/Data.csv" ;
-my $call2 = "./createHeatmap.pl GER $kw HeatMap_GER_$datum ../$GER_2021/Data.csv ../$GER/Data.csv" ;
+my $call1 = "./createHeatmap.pl -c $configFile -o MUC -w $kw -f HeatMap_MUC_$datum\.gif ../../../$MUC_2021/Data.csv ../../../$MUC/Data.csv" ;
+my $call2 = "./createHeatmap.pl -c $configFile -o GER -w $kw -f HeatMap_GER_$datum\.gif ../../../$GER_2021/Data.csv ../../../$GER/Data.csv" ;
+# my $call2 = "./createHeatmap.pl GER $kw HeatMap_GER_$datum ../$GER_2021/Data.csv ../$GER/Data.csv" ;
 print "call: ".$call1."\n";
 print `$call1`."\n";
 print "call: ".$call2."\n";
 print `$call2`."\n";
 # print `./createHeatmap.pl GER $kw 'HeatMap_GER_$datum' ../$GER_2021/Data.csv ../$GER/Data.csv`."\n";
 
-print `mv *MUC*.gif ../MUC_GIF/`."\n";
-print `mv *GER*.gif ../GER_GIF/`."\n";
+# print `mv *MUC*.gif ../MUC_GIF/`."\n";
+# print `mv *GER*.gif ../GER_GIF/`."\n";
 # print `mv *MUC*.jpeg ../MUC_JPG/`."\n";
 # print `mv *GER*.jpeg ../GER_JPG/`."\n";
 
