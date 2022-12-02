2109f994d3cd79feaf0f6fc54576be7e2c8aedf1
neko <neko@greenie.muc.de>
2022-12-02 08:27:08 +0100

    added script to create mastodon images (600x800 or 800x600)
    syn: ./make_mastodonImages.pl <infile> <outfile>
    
    example: ./make_mastodonImages.pl ../MUC_GIF/2022_12_01_47.gif ../MUC_GIF/2022_12_01_47_mast.gif

---
+++ bin/make_mastodonImages.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use lib qw(
+            /Users/neko/perl5/lib/perl5/darwin-thread-multi-2level
+            /Users/neko/perl5/lib/perl5
+        );
+
+# scale images to something in the range of: 600x800 / 800x600
+
+use Math::Round qw( nearest nlowmult );
+use Data::Dumper;
+# use Encoding qw(decode encode);
+
+use Imager;
+use Imager::Fill;
+# use Imager::Font;
+
+my $infile = shift || die "kein file bekommen\n";
+my $use_this_outputname = shift // 'mast';
+
+print("verwende: $infile\n");
+my $image = Imager->new;
+$image->read(file => $infile) or die $image->errstr;
+
+
+my $fn = $use_this_outputname.'.gif';
+print "output: $fn\n";
+
+my $newimg = $image->scale(xpixels=>800,ypixels=>600,type=>'min');
+
+# Check if given format is supported
+if ( $Imager::formats{'gif'} ) {
+    
+    $newimg->write( file=>$fn );
+    if ( $newimg->errstr ) {
+        die "Fehler beim exportieren: ".$newimg->errstr ."\n";
+    }
+    # $saved = 1;
+
+}
+
+
+exit 0;
