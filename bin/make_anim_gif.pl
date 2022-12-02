9aad4cc3b2a1e84f136d03639e97c61de6d1bbb9
neko <neko@greenie.muc.de>
2022-12-02 08:24:58 +0100

    added new Script for creating animated gifs.
    Syn:
    
    ./make_anim_gif.pl <indir> <outputfile>
    
    example: ./make_anim_gif.pl ../MUC_GIF/ mucanim

---
+++ bin/make_anim_gif.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use lib qw(
+            /Users/neko/perl5/lib/perl5/darwin-thread-multi-2level
+            /Users/neko/perl5/lib/perl5
+        );
+
+
+use Math::Round qw( nearest nlowmult );
+use Data::Dumper;
+# use Encoding qw(decode encode);
+
+use Imager;
+use Imager::Fill;
+# use Imager::Font;
+
+my $indir = shift || die "kein Directory bekommen\n";
+my $use_this_outputname = shift // 'anim_test';
+
+my @files = ();
+my @images = ();
+opendir ( DIR, $indir ) || die "Error in opening dir $indir\n";
+
+while( ( my $filename = readdir(DIR))) {
+    push @files, $filename;
+}
+closedir(DIR);
+
+
+foreach my $file ( sort @files ){
+    next unless $file;
+    next if $file =~ /^\.+/;
+    next if $file =~ /^\.DS/;
+
+    print("verwende: $indir/$file\n");
+    my $image = Imager->new;
+    $image->read(file => $indir.'/'.$file) or die $image->errstr;
+
+    push @images, $image;
+}
+
+my $filename = $use_this_outputname.'.gif';
+print "output: $filename\n";
+
+Imager->write_multi({ file=>$filename, type=>'gif' }, @images)
+    or die Imager->errstr;
+
+exit 0;
